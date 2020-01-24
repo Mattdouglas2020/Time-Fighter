30e4b2702791d3746bda1f73a622f4ecb34886be
Matthew Douglas <mattmardoug@gmail.com>
2020-01-23 23:53:56 -0600

    uploading to play store look into

--- app/src/main/java/com/mattdouglas/timefighter/MainActivity.kt
+++ app/src/main/java/com/mattdouglas/timefighter/MainActivity.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.raywenderlich.timefighter.BuildConfig
 import com.raywenderlich.timefighter.R
 
 class MainActivity : AppCompatActivity() {
